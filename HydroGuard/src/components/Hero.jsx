73591caa1e77d74bf551577d3fdb132b59a1765c
IT20159030 <it20159030@my.sliit.lk>
2024-10-14 16:37:29 +0530

    Refactor Hero component: Update link href

--- HydroGuard/src/components/Hero.jsx
+++ HydroGuard/src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               <a
-                href="#"
+                href="#literature-survey"
                 className="rounded-md bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm transition-transform transform hover:bg-gray-800 hover:shadow-lg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
               >
                 Learn more <span aria-hidden="true">→</span>
