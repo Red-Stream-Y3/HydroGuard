11a797ded744fbb2d5d1bf63ef4ddbb4b316927c
IT20159030 <it20159030@my.sliit.lk>
2024-10-29 22:04:18 +0530

    Refactor Literature component styles in HydroGuard

--- HydroGuard/src/components/Literature.jsx
+++ HydroGuard/src/components/Literature.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Literature = () => {
   return (
-    <div id="literature-survey" className="py-6 lg:py-10">
+    <div id="literature-survey" className="py-6 lg:py-16">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Literature Survey
